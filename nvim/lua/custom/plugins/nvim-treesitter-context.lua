8bd49f16d63d72c59083a005184fba59c1583a1b
Kurt Hu <kurthu2@gmail.com>
2024-10-15 01:29:56 -0400

    add nvim

---
+++ nvim/lua/custom/plugins/nvim-treesitter-context.lua
@@ -0,0 +1,9 @@
+return {
+  'nvim-treesitter/nvim-treesitter-context',
+  opts = {
+    max_lines = 5,
+  },
+  dependencies = {
+    'nvim-treesitter/nvim-treesitter',
+  },
+}
---
+++ nvim/test.js
@@ -0,0 +1,8 @@
+function hi(a, b) {
+  return a + b;
+}
+
+hi(2, 3);
+
+console.log("hi");
+console.log("hi");
